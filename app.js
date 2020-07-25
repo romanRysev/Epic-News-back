bdb4c7bad28e6c009e80ba648d1e889052d9ef0e
Roman Rysev <roman.rysev@gmail.com>
2020-07-25 14:34:34 +0300

    add CORS

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const { PORT, DB_ADRESS } = require('./config');
 const routes = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const error = require('./middlewares/errors.js');
 const limiter = require('./middlewares/limiter');
+
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
 const app = express();
 
@@ -19,6 +27,7 @@ mongoose.connect(DB_ADRESS, {
 });
 
 app.use(limiter);
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
