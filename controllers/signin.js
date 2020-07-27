b624e0e0e4464231f3bb960eb16b4fb5d218434f
Roman Rysev <roman.rysev@gmail.com>
2020-07-27 20:41:09 +0300

    signon.js - добавил возможность выхода из профиля

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,34 +1,43 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-const User = require('../models/user');
-const UnauthorizedError = require('../errors/UnauthorizedError');
-const { loginErrorMessage } = require('../const');
-const config = require('../config');
+const User = require("../models/user");
+const UnauthorizedError = require("../errors/UnauthorizedError");
+const { loginErrorMessage } = require("../const");
+const config = require("../config");
 
 const signIn = (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password, mode } = req.body;
 
-  User.findOne({ email }).select('+password')
-    .then((user) => {
-      if (!user) {
-        throw new UnauthorizedError(loginErrorMessage);
-      }
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            throw new UnauthorizedError(loginErrorMessage);
-          }
-          const token = jwt.sign({ _id: user._id }, config.JWT_SECRET, { expiresIn: '7d' });
-          res.cookie('jwt', token, {
-            maxAge: 3600000 * 24 * 7,
-            httpOnly: true,
+  if (mode == "logout") {
+    res.clearCookie("jwt").send({ Message: "Выход из профиля" });
+  } else {
+    User.findOne({ email })
+      .select("+password")
+      .then((user) => {
+        if (!user) {
+          throw new UnauthorizedError(loginErrorMessage);
+        }
+        return bcrypt
+          .compare(password, user.password)
+          .then((matched) => {
+            if (!matched) {
+              throw new UnauthorizedError(loginErrorMessage);
+            }
+            const token = jwt.sign({ _id: user._id }, config.JWT_SECRET, {
+              expiresIn: "7d",
+            });
+            res
+              .cookie("jwt", token, {
+                maxAge: 3600000 * 24 * 7,
+                httpOnly: true,
+              })
+              .send({ Message: "OK" });
           })
-            .send({'Message': 'OK'});
-        })
-        .catch(next);
-    })
-    .catch(next);
+          .catch(next);
+      })
+      .catch(next);
+  }
 };
 
 module.exports = signIn;
